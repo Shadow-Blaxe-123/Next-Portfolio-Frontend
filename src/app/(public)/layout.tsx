59059b097834d6cdf987824b79b444290aa9208f
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-10-01 23:55:25 +0600

    made navbar sticky

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function PublicLayout({ children }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
-      <div>
+      <div className="sticky top-0 z-50">
         <Navbar />
       </div>
       <main className="flex-1">{children}</main>
