a83ff37baff210279ffdb40156205d64469c8088
Shadow-Blaxe-123 <shamunahmed38@gmail.com>
2025-10-25 23:17:56 +0600

    Dashboard sidebar added

--- src/app/(private)/dashboard/layout.tsx
+++ src/app/(private)/dashboard/layout.tsx
@@ -1,11 +1,28 @@
+import { AppSidebar } from "@/components/app-sidebar";
+
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 import { PageLayoutProps } from "@/interface";
 
-function Dashboardlayout({ children }: PageLayoutProps) {
+export default function AppLayout({ children }: PageLayoutProps) {
   return (
-    <div>
-      <h1>This is layout component.</h1>
-      <main>{children}</main>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </div>
+        </header>
+
+        {/* Page content */}
+        <main className="flex flex-1 flex-col gap-4 p-4">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
-export default Dashboardlayout;
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,85 @@
+import * as React from "react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+  SidebarRail,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Logo from "./Logo";
+
+// This is sample data.
+const data = {
+  navMain: [
+    { title: "Dashboard", url: "/dashboard" },
+    {
+      title: "Projects",
+      url: "#",
+      items: [
+        { title: "All Projects", url: "/dashboard/projects" },
+        { title: "Create Project", url: "/dashboard/projects/new" },
+      ],
+    },
+    {
+      title: "Blogs",
+      url: "#",
+      items: [
+        { title: "All Blogs", url: "/dashboard/blogs" },
+        { title: "Create Blog", url: "/dashboard/blogs/new" },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/" className="text-primary hover:text-primary/90">
+                <Logo />
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            {data.navMain.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url} className="font-medium">
+                    {item.title}
+                  </a>
+                </SidebarMenuButton>
+                {item.items?.length ? (
+                  <SidebarMenuSub>
+                    {item.items.map((item) => (
+                      <SidebarMenuSubItem key={item.title}>
+                        <SidebarMenuSubButton asChild>
+                          <a href={item.url}>{item.title}</a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                ) : null}
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
